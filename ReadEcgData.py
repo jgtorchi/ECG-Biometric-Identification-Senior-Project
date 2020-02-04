0168afb12f58a4db058f85f632dfc2377441b997
jgtorchi <jtorchia.lcjh@gmail.com>
2020-02-04 00:38:16 -0800

    Added the ECG data and the python script to read in the ECG data into a NumPy array.

---
+++ ReadEcgData.py
@@ -0,0 +1,14 @@
+import wfdb
+import matplotlib.pyplot as plt
+#record = wfdb.rdrecord('ecg-id-database-1.0.0/Person_01/rec_1', sampto=3000)
+#annotation = wfdb.rdann('ecg-id-database-1.0.0/Person_01/rec_1', 'atr', sampto=3000)
+#wfdb.plot_wfdb(record=record, annotation=annotation, plot_sym=True,
+#                   time_units='seconds', title='Person 1, Record 1')
+signals, fields = wfdb.rdsamp('ecg-id-database-1.0.0/Person_01/rec_1',sampto=3000)
+print(signals)
+print(signals[:,1])
+plt.figure(1)
+fig = plt.plot(signals[:,1])
+plt.ylabel('mV')
+plt.xlabel('seconds')
+plt.show()
